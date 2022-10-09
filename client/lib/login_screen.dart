8cde35e9b3e4ae63cbce8c3edba90136bf33bc98
KhoaAssassin <61230907+KhoaAssassin6954@users.noreply.github.com>
2022-10-09 23:24:37 +0700

    Login_Screen hide password Register_screen text color

---
+++ client/lib/login_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:oktoast/oktoast.dart';
+import 'package:untitled_folder/register_screen.dart';
+import 'package:untitled_folder/widget.dart';
+
+import 'ValidateUtil.dart';
+import 'inittial_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({Key? key}) : super(key: key);
+
+  final margin = 0.0;
+  var formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WillPopScope(
+        onWillPop: () async {
+          Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(
+                  builder: (BuildContext context) => const InittialScreen()),
+              ModalRoute.withName('/'));
+          return true;
+        },
+        child: Form(
+          key: formkey,
+          child: Column(
+            children: [
+              headerScreen("Sign up", () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => RegisterScreen()));
+              }),
+              Expanded(
+                  child: Body(
+                [
+                  Container(
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Login",
+                      style: _textStyle,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ...itemTextFormField("Username", (value) {
+                    return ValidateUtil.isNameUser(value)
+                        ? null
+                        : "Username Invalid";
+                  }).children,
+                  ...itemTextFormField("Password", (value) {
+                    return ValidateUtil.isPassUser(value)
+                        ? null
+                        : "Password Invalid";
+                  }, obscureText: true)
+                      .children,
+                  button("Login", () {
+                    if (formkey.currentState?.validate() == false) {
+                      showToast('Format Invalid',
+                          position: ToastPosition.bottom);
+                    }
+                  },
+                      textColor: Colors.white,
+                      margin: const EdgeInsets.only(top: 20))
+                ],
+                alignment: CrossAxisAlignment.start,
+              ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  TextStyle get _textStyle => const TextStyle(
+      fontWeight: FontWeight.bold, fontSize: 18, color: Colors.black);
+}
